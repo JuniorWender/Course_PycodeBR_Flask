c39d3ec7f02351482f6c11cb28d7ded4cf70aa82
wender.junior <wender.junior@etaure.com.br>
2023-05-22 08:57:17 -0300

    Get and Post

---
+++ dataBase.py
@@ -0,0 +1,45 @@
+Carros = [  
+    {  
+       "id":1,
+       "marca":"Chevrolet",
+       "modelo":"Agile LT 1.4 MPFI 8V flex",
+       "valor":21500,
+       "unidadesEmEstoque":5,
+       "ano":2010,
+       "categoria":"carro",
+       "imagemUrl":"https://img.olx.com.br/images/67/671816000602508.jpg"
+    },
+    {  
+       "id":2,
+       "marca":"Dodge",
+       "modelo":"Dakota Durango SLT 5.2 4x4 V8",
+       "ano":1998,
+       "valor":28.470,
+       "unidadesEmEstoque":2,
+       "categoria":"carro",
+       "imagemUrl":"https://i.ytimg.com/vi/NtCOyk1KP5g/maxresdefault.jpg"
+ 
+    },
+    {  
+       "id":3,
+       "marca":"Fiat",
+       "modelo":"Idea Advent./ Adv.LOCKER 1.8 mpi Flex 5p",
+       "ano":2016,
+       "valor":50414,
+       "unidadesEmEstoque":2,
+       "categoria":"carro",
+       "imagemUrl":"https://http2.mlstatic.com/fiat-idea-advent-advlocker-18-mpi-flex-5p-D_NQ_NP_907181-MLB26413039072_112017-F.jpg"
+ 
+    },
+    {  
+       "id":4,
+       "marca":"Mitsubishi",
+       "modelo":"Eclipse GT 3.8 V6 267cv",
+       "ano":2011,
+       "valor":103469,
+       "unidadesEmEstoque":1,
+       "categoria":"carro",
+       "imagemUrl":"https://http2.mlstatic.com/mitsubishi-eclipse-gt-v6-2007-2008-D_NQ_NP_922101-MLB20283158988_042015-F.jpg"
+ 
+    }
+]
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('teste')
+from flask import Flask, make_response, request, jsonify # Primeiro é da onde está sendo importado, segundo é o que está sendo importado
+from dataBase import Carros
+
+app = Flask(__name__)
+
+@app.route("/carros", methods=["GET"])
+def get_carros():
+    return make_response(jsonify(Carros))
+
+@app.route("/carros", methods=["POST"])
+def create_carro():
+    NovoCarro = request.json
+    Carros.append(NovoCarro)
+    return NovoCarro
+
+
+app.run()
