5c4efaee33e04bee9e0f9b1432db65156d766f3b
Junior Wender <juniorwender1996@gmail.com>
2023-05-24 12:31:27 -0300

    Basic Flask Api

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('teste')
+from flask import Flask, make_response, request, jsonify # Primeiro é da onde está sendo importado, segundo é o que está sendo importado
+from dataBase import Carros
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+@app.route('/carros', methods=['GET'])
+def get_Carros():
+    return make_response(jsonify(message='Lista de Carros',data=Carros))
+
+@app.route("/carros", methods=["POST"])
+def create_carro():
+    NovoCarro = request.json
+    Carros.append(NovoCarro)
+    return make_response(jsonify(message='Carro Adicionado Com Sucesso!',data=NovoCarro))
+
+app.run()
